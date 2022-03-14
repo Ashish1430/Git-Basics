391502e3097306f9485ed4539eb4b0a2e509efb1
Ashish Biswakarma <ashishbiswakarma6@gmail.com>
2022-03-14 13:05:32 +0530

    added massage in sum.java

---
+++ src/com/dsa/graph/Dfs_Implementation.java
@@ -0,0 +1,7 @@
+package com.dsa.graph;
+
+public class Dfs_Implementation {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/dsa/graph/Main.java
+++ src/com/dsa/graph/Main.java
@@ -1,8 +1,11 @@
-public class sum {
+package com.dsa.graph;
+
+public class Main {
+
     public static void main(String[] args) {
+	// write your code here
         int a=2;
         int b= 3;
-        System.out.println("The sum is");
         System.out.println(a+b);
     }
 }
